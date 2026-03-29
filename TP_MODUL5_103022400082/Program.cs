844651e814de204687c91df625f6811e6228fed7
Erleazar Pandita Ramadhan <erleazar02@gmail.com>
2026-03-30 01:08:37 +0700

    resolve conflict

--- TP_MODUL5_103022400082/Program.cs
+++ TP_MODUL5_103022400082/Program.cs
@@ -1,1 +1,42 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine("Halo user " + user);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser<string>("Erleazar"); 
+    }
+}
+
+class DataGeneric<T>
+{
+    public T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+
+static void Main(string[] args)
+{
+    HaloGeneric halo = new HaloGeneric();
+    halo.SapaUser<string>("Erleazar");
+
+    DataGeneric<string> data = new DataGeneric<string>("103022400082");
+    data.PrintData();
+}
